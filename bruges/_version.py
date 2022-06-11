b90e613b367d694fe16ceeccc0dd724d0283ccff
Alex Dunbar <alex.b.dunbar@gmail.com>
2022-06-11 08:18:14 +0200

    spring cleaning deletions

--- bruges/_version.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Version.
-
-Doing it this way provides for access in setup.py and via __version__
-"""
-__version__ = "0.4.0"
--- bruges/noise/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .noise import noise_db
